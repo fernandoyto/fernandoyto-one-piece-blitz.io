110c6080f6442f23250b411a3486c8014da5e422
Fernando Yto <fernando.toshioyto@gmail.com>
2020-02-19 21:29:27 -0300

    game classes refactored

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,68 +1,86 @@
-const wantedName = ['luffy', 'ace', 'sabo', 'chopper', 'law'];
-const wantedHat = ['luffy-hat', 'ace-hat', 'sabo-hat', 'chopper-hat', 'law-hat'];
+// const wantedName = ['luffy', 'ace', 'sabo', 'chopper', 'law'];
+// const wantedHat = ['luffy-hat', 'ace-hat', 'sabo-hat', 'chopper-hat', 'law-hat'];
 const wantedAnswer = ['Q', 'W', 'E', 'A', 'S'];
-let correctAnswers = 0;
+// let correctAnswers = 0;
 
-class WantedHatCombo {
-  constructor(wantedArray, hatArray) {
-    const copyWantedName = [...wantedArray];
-    const copyWantedHat = [...hatArray];
+class OnePieceBlitz {
+  constructor() {
+    this.wantedName = ['luffy', 'ace', 'sabo', 'chopper', 'law'];
+    this.wantedHat = ['luffy-hat', 'ace-hat', 'sabo-hat', 'chopper-hat', 'law-hat'];
+    this.wanted1 = '';
+    this.hat1 = '';
+    this.wanted2 = '';
+    this.hat2 = '';
+    this.answerIndex = 0;
+    this.correctWanted = '';
+    this.correctHat = '';
+    this.indexArray = [];
+    this.correctAnswers = 0;
+  }
 
-    const indexArray = [];
-    copyWantedName.forEach((element, index) => {
-      indexArray.push(index);
-    });
+  defineAnswer() {
+    this.answerIndex = Math.floor(Math.random() * this.wantedName.length);
+    this.correctWanted = this.wantedName[this.answerIndex];
+    this.correctHat = this.wantedHat[this.answerIndex];
+  }
 
-    const answerIndex = Math.floor(Math.random() * indexArray.length);
-    this.correctWanted = copyWantedName[answerIndex];
-    this.correctHat = copyWantedHat[answerIndex];
-
+  defineCombo() {
     let randomIndex;
+    this.wantedName.forEach((element, index) => {
+      this.indexArray.push(index);
+    });
 
     // randomly decide if card will have zero correct attributes or 1 fully correct element
     if (Math.random() >= 0.5) {
-      // if true, then it will have 1 fully correct element
-      this.wanted1 = copyWantedName[answerIndex];
-      this.hat1 = copyWantedHat[answerIndex];
+      // display 1 fully correct element
+      this.wanted1 = this.wantedName[this.answerIndex];
+      this.hat1 = this.wantedHat[this.answerIndex];
 
-      indexArray.splice(indexArray.indexOf(answerIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      // remove possibility of repeating cards by removing used index
+      this.indexArray.splice(this.indexArray.indexOf(this.answerIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.wanted2 = copyWantedName[randomIndex];
+      this.wanted2 = this.wantedName[randomIndex];
 
-      indexArray.splice(indexArray.indexOf(randomIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      this.indexArray.splice(this.indexArray.indexOf(randomIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.hat2 = copyWantedHat[randomIndex];
+      this.hat2 = this.wantedHat[randomIndex];
     } else {
-      // if false, then it will have zero correct elements
-      indexArray.splice(indexArray.indexOf(answerIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      // display have zero correct elements
+      this.indexArray.splice(this.indexArray.indexOf(this.answerIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.wanted1 = copyWantedName[randomIndex];
+      this.wanted1 = this.wantedName[randomIndex];
 
-      indexArray.splice(indexArray.indexOf(randomIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      this.indexArray.splice(this.indexArray.indexOf(randomIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.hat1 = copyWantedHat[randomIndex];
+      this.hat1 = this.wantedHat[randomIndex];
 
-      indexArray.splice(indexArray.indexOf(randomIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      this.indexArray.splice(this.indexArray.indexOf(randomIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.wanted2 = copyWantedName[randomIndex];
+      this.wanted2 = this.wantedName[randomIndex];
 
-      indexArray.splice(indexArray.indexOf(randomIndex), 1);
-      randomIndex = indexArray[Math.floor(Math.random() * indexArray.length)];
+      this.indexArray.splice(this.indexArray.indexOf(randomIndex), 1);
+      randomIndex = this.indexArray[Math.floor(Math.random() * this.indexArray.length)];
 
-      this.hat2 = copyWantedHat[randomIndex];
+      this.hat2 = this.wantedHat[randomIndex];
     }
   }
 
   isCorrect(pickedWanted) {
     if (pickedWanted.wanted === this.correctWanted && pickedWanted.hat === this.correctHat) {
+      this.correctAnswers += 1;
       return true;
     }
     return false;
+  }
+
+  gameStart() {
+    this.defineAnswer();
+    this.defineCombo();
   }
 }
 
